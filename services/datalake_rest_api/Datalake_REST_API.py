92eff729f8d4dd9d1fea00013ab78277d43f1103
Terje <terje.bakkelokken@evry.com>
2022-02-07 14:24:45 +0100

    initial commit

--- services/datalake_rest_api/Datalake_REST_API.py
+++ services/datalake_rest_api/Datalake_REST_API.py
@@ -13,6 +13,12 @@ https://www.youtube.com/watch?v=oLdEI3zUcFg
 https://azure.microsoft.com/en-us/services/app-service/api/
 https://docs.microsoft.com/en-us/azure/app-service/overview-security
 
+Various tips:
+https://dev.to/fuadrafid/fastapi-the-good-the-bad-and-the-ugly-20ob
+
+TODO: Pagination (DB only), delta, search:
+https://pypi.org/project/fastapi-pagination/
+
 Run app with: uvicorn Datalake_REST_API:app --reload
 App: http://127.0.0.1:8000
 Swagger: http://127.0.0.1:8000/docs
@@ -20,46 +26,47 @@ Swagger: http://127.0.0.1:8000/docs
 
 # ------------------------------------------------------------------------------------------------------------
 
-from typing import Optional, List
+from typing import Optional, List, Union
 
 from datetime import datetime
 import uuid
 import math
 import json
 import timeit
-import jsonschema
-from jsonschema import validate
+# import jsonschema
+import pandas as pd
+# from jsonschema import validate
 from urllib.parse import unquote
 import os
 from os.path import exists
-import pandas as pd
+# import pandas as pd
 import io
 import time
-import yaml
+# import yaml
 import collections
-import scipy.stats
-from scipy.stats import kurtosis, skew
-import dicttoxml
-from dict2xml import dict2xml
-from dicttoxml import dicttoxml
-import xml.etree.ElementTree as ET
+# import scipy.stats
+# from scipy.stats import kurtosis, skew
+# import dicttoxml
+# from dict2xml import dict2xml
+# from dicttoxml import dicttoxml
+# import xml.etree.ElementTree as ET
 
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
 
 from starlette.responses import StreamingResponse, JSONResponse, HTMLResponse, Response
 from starlette.middleware.cors import CORSMiddleware
-from fastapi import FastAPI, Body, Path, Query, Cookie, Header
+from fastapi import FastAPI, Body, Path, Query, Cookie, Header, HTTPException
 from pydantic import BaseModel, Field, EmailStr
 
 # For Azure Blob Storage:
-from azure.storage.blob import BlobServiceClient, ContainerClient, BlobClient
-# from azure.storage.blob.aio import BlobClient
+from azure.storage.blob import BlobServiceClient, ContainerClient # , BlobClient
+from azure.storage.blob.aio import BlobClient
 from azure.core.exceptions import ResourceNotFoundError
 
-BytesIO = pd.io.common.BytesIO
-StringIO = pd.io.common.StringIO
+# BytesIO = pd.io.common.BytesIO
+# StringIO = pd.io.common.StringIO
 
 # ------------------------------------------------------------------------------------------------------------
 
@@ -72,7 +79,7 @@ app = FastAPI(title='DataFabrikken 2.0 Datalake Demo API')
 # )
 # SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+# Base = declarative_base()
 
 origins = [
     '*'
@@ -115,38 +122,44 @@ class MyDataLake():
         return None
             
     
-    async def get_data(self, blob_name, sep=';', encoding='iso-8859-1', header=1, download=False):
+    async def get_data(self, blob_name, page_no=None, per_page=None):
         if not self.FOLDER in blob_name:
             blob_name = self.FOLDER + blob_name
         # print(blob_name)
         blob_client = self.container_client.get_blob_client(blob_name)
-        blob_content = blob_client.download_blob().readall() # Som bytes
+        # blob_exists = blob_client.exists()
+        try:
+            blob_content = blob_client.download_blob().readall() # Som bytes
+            blob_exists = True
+        except:
+            blob_exists = False
         # blob_content = blob_client.download_blob().content_as_text()
         # stream = await blob_client.download_blob()
         # data = await stream.readall()
         
         # TODO: We need to check that the blob actually exists, and give a better error message (not exception dump)
         
-        if header <= 0:
-            header = 1
-            
+        if not blob_exists:
+            return False
+        
         blob_name = blob_name.upper()
         if not 'ZIP' in blob_name and not 'GZ' in blob_name:
             if 'CSV' in blob_name:
                 if self.DEBUG:
                     print('Getting CSV file...')
-                return blob_content # TEST
             elif 'XLS' in blob_name:
                 if self.DEBUG:
                     print('Getting Excel file...')
-                return blob_content # TEST
             elif 'JSON' in blob_name:
                 if self.DEBUG:
                     print('Getting JSON file...')
-                return blob_content # TEST
-                     
-                self.df = pd.read_json(BytesIO(blob_content), header=header-1) # Convert bytes-array to pandas DataFrame 
-                return self.df.to_dict()
+
+            if page_no:
+                # TODO: Validate range of page_no and per_page (not negative, etc.)
+                # TEST: Need to find linefeed chars to detect number of lines in raw text buffer?
+                return blob_content # Default for now
+            else:
+                return blob_content
         else:
             if 'GZ' in blob_name:
                 if self.DEBUG:
@@ -157,7 +170,6 @@ class MyDataLake():
 # -----------------------------------------------------------------------------------------------------------------------------
 
 dl_obj = MyDataLake()
-
 
 
 @app.on_event('startup')
@@ -172,20 +184,37 @@ async def shutdown():
 
 @app.get('/')
 async def root():
-    return {'message': 'DataFabrikken 2.0 API. See <url>/docs for documentation about the API'}
+    return {'message': 'DataFabrikken 2.0 API. See <url>/docs for Swagger API documentation'}
 
-# Return dataset for download if ?download parameter present, otherwise return dataframe
+# Return dataset content or HTTPException if not found
 # https://fastapi.tiangolo.com/tutorial/query-params/
 # https://fastapi.tiangolo.com/advanced/custom-response/
 @app.get('/datasets/{dataset_name}')
-async def download_dataset(dataset_name:str) -> Response:
+async def download_dataset(dataset_name:str, page_no:Optional[int]=None, per_page:Optional[int]=30) -> Union[Response, HTTPException]:
     dataset_name = unquote(dataset_name)
     print('Dataset name:', dataset_name)
     # return dataset_name
 
-    dataset = await dl_obj.get_data(f'{dataset_name}')
-    return Response(content=dataset, media_type='text/csv') # TODO: Check file extension and return correct media type!
+    if '.XLS' in dataset_name.upper():
+        extension = 'excel'
+    if '.CSV' in dataset_name.upper():
+        extension = 'csv'
+    if '.XML' in dataset_name.upper():
+        extension = 'xml'
+    if '.JSON' in dataset_name.upper():
+        extension = 'json'
 
-# Just for localhost
+    try:
+        dataset = await dl_obj.get_data(f'{dataset_name}', page_no=page_no, per_page=per_page)
+    except Exception as e:
+        return HTTPException(status_code=500, detail=f'An error occured: {e.message}')
+       
+    if not dataset:
+        return HTTPException(status_code=404, detail=f'File \'{dataset_name}\' does not exist.')
+
+    return Response(content=dataset, media_type=f'text/{extension}')
+ 
+
+# For testing on localhost
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
